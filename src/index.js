e9a2c00737d9a4805eda5411e37bd5ac80cc4d43
Julia.K <knaeble.julia@gmail.com>
2020-08-20 11:03:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 import Thunk from "redux-thunk";
 
 const myStore = createStore(
-  allReducer,
+  allReducer(),
   applyMiddleware(Thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
