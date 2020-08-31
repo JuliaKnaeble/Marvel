9ff8b8b7fe8d536fe7811db166e50f15fdb4a8e5
Julia.K <knaeble.julia@gmail.com>
2020-08-31 12:23:49 +0200

    Satrted new search implementation

--- src/Navigation/NavbarNew.js
+++ src/Navigation/NavbarNew.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarNew.scss";
 
 const NavbarNew = () => {
+  const [searchClicked, setSearchClicked] = useState(false);
+
+  let menu;
+  if (searchClicked) {
+    menu = (
+      <div className="menu-opene">
+        <form>
+          <input
+            type="text"
+            placeholder="Search your Hero"
+            autoComplete="off"
+            spellCheck="false"
+            autoFocus
+          />
+        </form>
+      </div>
+    );
+  }
+
   return (
-    <div className="NavbarNew">
-      <span className="top">1</span>
-      <span>2</span>
-      <span className="bottom">3</span>
+    <div>
+      {menu}
+      <div className="NavbarNew">
+        <span className="top">1</span>
+        <span onClick={() => setSearchClicked(!searchClicked)}>2</span>
+        <span className="bottom">3</span>
+      </div>
     </div>
   );
 };
