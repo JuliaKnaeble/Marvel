687ebd967d0b15e50004dc07a9f5763b2e38e734
Julia.K <knaeble.julia@gmail.com>
2020-09-26 17:21:23 +0200

    initial setup for comicsdetail component

---
+++ src/Components/ComicsDetail/ComicsDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./ComicsDetail.scss";
+
+const ComicsDetail = () => {
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+};
+
+export default ComicsDetail;
