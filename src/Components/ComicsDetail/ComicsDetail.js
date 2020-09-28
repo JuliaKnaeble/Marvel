5101e9935b8b72b2999c68fc37d764be51528a46
Julia.K <knaeble.julia@gmail.com>
2020-09-28 18:03:27 +0200

    adding detail to comic detail page

--- src/Components/ComicsDetail/ComicsDetail.js
+++ src/Components/ComicsDetail/ComicsDetail.js
@@ -16,11 +16,32 @@ const ComicsDetail = ({ comic, hero }) => {
           alt=""
         ></img>
       </div>
-      <div className="comic-description">
+      <div className="comic-details">
         <p className="lable">
           <span>{hero.name}</span>
         </p>
         <h3>{comic[0].title}</h3>
+        <h4>
+          {comic[0].description !== null
+            ? comic[0].description
+            : `Sadly this comic has no description.`}
+        </h4>
+        <div className="comic-dates">
+          <p>
+            <span>
+              ORIGINAL PRICE:
+              <span className="detail">{comic[0].prices[0].price}</span>
+            </span>
+            <span>
+              DATE:
+              <span className="detail"></span>
+            </span>
+            <span>
+              PAGES:
+              <span className="detail">{comic[0].pageCount}</span>
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
