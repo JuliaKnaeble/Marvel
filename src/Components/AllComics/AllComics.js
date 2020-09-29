95dbda9b1cf7e335928085c18f9dcf24697f308a
Julia.K <knaeble.julia@gmail.com>
2020-09-29 17:05:07 +0200

    basic setup all comics

---
+++ src/Components/AllComics/AllComics.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AllComics.scss";
+
+const AllComics = () => {
+  return (
+    <div>
+      <p>All Comics</p>
+    </div>
+  );
+};
+
+export default AllComics;
