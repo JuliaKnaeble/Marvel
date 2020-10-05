0688bbdfc789bc6f6df6591bfbc6204ad37127c7
Julia.K <knaeble.julia@gmail.com>
2020-10-05 18:14:45 +0200

    created HomeBg component

---
+++ src/Components/HomePage/HomeBg.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomeBg.scss";
+import Home from "./Home";
+
+const HomeBg = () => {
+  return (
+    <div className="HomeBg">
+      <Home />
+    </div>
+  );
+};
+
+export default HomeBg;
