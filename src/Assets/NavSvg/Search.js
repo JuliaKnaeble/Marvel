169a2562c782550657ceb07e93409ae1ed18b346
Julia.K <knaeble.julia@gmail.com>
2020-10-13 10:39:16 +0200

    initial setup for svg navigation

---
+++ src/Assets/NavSvg/Search.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Search = ({ className }) => {
+  return (
+    <svg
+      className={className}
+      width="37px"
+      height="37px"
+      viewBox="0 0 37 37"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <title>08 Assets / General 24x24 / Search Copy@2x</title>
+      <defs>
+        <path
+          d="M22,6 C30.836556,6 38,13.163444 38,22 C38,25.6974492 36.7458189,29.1019687 34.6396699,31.8113455 L41.8994949,39.0710678 C42.6805435,39.8521164 42.6805435,41.1184464 41.8994949,41.8994949 C41.1184464,42.6805435 39.8521164,42.6805435 39.0710678,41.8994949 L31.8113455,34.6396699 C29.1019687,36.7458189 25.6974492,38 22,38 C13.163444,38 6,30.836556 6,22 C6,13.163444 13.163444,6 22,6 Z M22,10 C15.372583,10 10,15.372583 10,22 C10,28.627417 15.372583,34 22,34 C28.627417,34 34,28.627417 34,22 C34,15.372583 28.627417,10 22,10 Z"
+          id="path-1"
+        ></path>
+      </defs>
+      <g
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <g
+          id="08-Assets-/-General-24x24-/-Search-Copy"
+          transform="translate(-6.000000, -6.000000)"
+        >
+          <mask id="mask-2" fill="white">
+            <use xlinkHref="#path-1"></use>
+          </mask>
+          <use
+            id="Combined-Shape"
+            fill="#000000"
+            fill-rule="nonzero"
+            xlinkHref="#path-1"
+          ></use>
+          <g id="Group" mask="url(#mask-2)">
+            <g transform="translate(2.000000, 2.000000)" id="🎨-Icon-Color">
+              <rect
+                id="🎨-Color"
+                fill="#000000"
+                x="0"
+                y="0"
+                width="44"
+                height="44"
+              ></rect>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default Search;
