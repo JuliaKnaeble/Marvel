495a8009472298f71c12d2536e1ea4f02fb901f7
Julia.K <knaeble.julia@gmail.com>
2020-10-13 17:50:18 +0200

    svg cleanup

--- src/Assets/NavSvg/Search.js
+++ src/Assets/NavSvg/Search.js
@@ -8,47 +8,32 @@ const Search = ({ className }) => {
       height="37px"
       viewBox="0 0 37 37"
       version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
     >
-      <title>Search</title>
       <defs>
         <path
           d="M22,6 C30.836556,6 38,13.163444 38,22 C38,25.6974492 36.7458189,29.1019687 34.6396699,31.8113455 L41.8994949,39.0710678 C42.6805435,39.8521164 42.6805435,41.1184464 41.8994949,41.8994949 C41.1184464,42.6805435 39.8521164,42.6805435 39.0710678,41.8994949 L31.8113455,34.6396699 C29.1019687,36.7458189 25.6974492,38 22,38 C13.163444,38 6,30.836556 6,22 C6,13.163444 13.163444,6 22,6 Z M22,10 C15.372583,10 10,15.372583 10,22 C10,28.627417 15.372583,34 22,34 C28.627417,34 34,28.627417 34,22 C34,15.372583 28.627417,10 22,10 Z"
-          id="path-1"
+          id="path-2"
         ></path>
       </defs>
       <g
-        id="Page-1"
+        id="Page-2"
         stroke="none"
         strokeWidth="1"
         fill="none"
         fillRule="evenodd"
       >
-        <g
-          id="08-Assets-/-General-24x24-/-Search-Copy"
-          transform="translate(-6.000000, -6.000000)"
-        >
+        <g id="Search" transform="translate(-6.000000, -6.000000)">
           <mask id="mask-2" fill="white">
-            <use xlinkHref="#path-1"></use>
+            <use xlinkHref="#path-2"></use>
           </mask>
           <use
             id="Combined-Shape"
             fill="currentColor"
             fillRule="nonzero"
-            xlinkHref="#path-1"
+            xlinkHref="#path-2"
           ></use>
-          <g id="Group" mask="url(#mask-2)">
-            <g transform="translate(2.000000, 2.000000)" id="🎨-Icon-Color">
-              <rect
-                id="🎨-Color"
-                fill="currentColor"
-                x="0"
-                y="0"
-                width="44"
-                height="44"
-              ></rect>
-            </g>
+          <g id="Group-Search" mask="url(#mask-2)">
+            <g transform="translate(2.000000, 2.000000)" id="Icon-Color-2"></g>
           </g>
         </g>
       </g>
