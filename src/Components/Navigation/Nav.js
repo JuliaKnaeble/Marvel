0ed312c13d6e0d35abcfebf6ec02eff5b496e045
Julia.K <knaeble.julia@gmail.com>
2020-10-25 12:37:03 +0100

    inital setup menu icon mobile

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import NavDesk from "./NavDesk"
+import NavMobile from "./NavMobile";
 
 const Nav = ({ requestRandomHero, requestSearch }) => {
     return(
     <div>
     <NavDesk requestRandomHero={requestRandomHero} requestSearch={requestSearch}/>
+    <NavMobile />
     </div>
     )
 }
--- src/Components/Navigation/NavMobile.js
+++ src/Components/Navigation/NavMobile.js
@@ -1,9 +1,14 @@
 import React from "react"; 
+import "./NavMobile.scss";
 
 const NavMobile = () => {
     return (
-        <div>
-            <img/>
+        <div className="NavMobile">
+            <img
+                className="menu-icon"
+                src={require(`../../Assets/mobile_ham.png`)}
+                alt="menu"
+              />
         </div>
     )
 }
