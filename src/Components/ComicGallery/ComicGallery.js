4cd32f95d995b7e1f61d7e6f3208ae0c297c3e50
Julia.K <knaeble.julia@gmail.com>
2020-10-31 15:17:16 +0100

    clean-up

--- src/Components/ComicGallery/ComicGallery.js
+++ src/Components/ComicGallery/ComicGallery.js
@@ -15,7 +15,6 @@ const ComicGallery = ({ hero, comic }) => {
     const slicedComics = comic.slice(start, end);
     let arrayConcat = arrayForHoldingComics.concat(slicedComics);
     setComicsToShow([...comicsToShow, ...arrayConcat]);
-    console.log(slicedComics);
   };
 
   useEffect(() => {
@@ -26,7 +25,6 @@ const ComicGallery = ({ hero, comic }) => {
   const handleShowMoreComics = () => {
     loopWithSlice(ref.current, ref.current + comicsPerLoad);
     ref.current += comicsPerLoad;
-    console.log(ref.current);
   };
 
   return (
